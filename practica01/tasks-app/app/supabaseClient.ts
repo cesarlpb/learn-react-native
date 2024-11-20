d579484e1feaaffc0c62c89ae12d7b904653aef9
César <cesarlpb@gmail.com>
2024-11-20 23:37:00 +0100

    lectura de env vars

--- practica01/tasks-app/app/supabaseClient.ts
+++ practica01/tasks-app/app/supabaseClient.ts
@@ -1,7 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://<your-project-id>.supabase.co';
-const SUPABASE_KEY = '<public-anon-key>';
+const SUPABASE_URL = `${process.env.EXPO_PUBLIC_SUPABASE_URL}`;
+const SUPABASE_KEY = `${process.env.EXPO_PUBLIC_SUPABASE_KEY}`;
+
+if (!SUPABASE_URL || !SUPABASE_KEY) {
+  throw new Error('Las variables de entorno SUPABASE_URL y SUPABASE_KEY deben estar definidas.');
+}
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
