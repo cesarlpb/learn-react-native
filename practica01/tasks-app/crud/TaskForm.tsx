305920f4f7b554c8d57cd2ea570b43e60061f094
César <cesarlpb@gmail.com>
2024-11-24 23:18:06 +0100

    actualizo importación de supabaseClient para usar alias @app

--- practica01/tasks-app/crud/TaskForm.tsx
+++ practica01/tasks-app/crud/TaskForm.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
-import supabase from '../app/supabaseClient';
+import supabase from '@app/supabaseClient';
 
 interface TaskFormProps {
   onSave: () => void;
--- practica01/tasks-app/crud/TaskList.tsx
+++ practica01/tasks-app/crud/TaskList.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, FlatList } from 'react-native';
-import supabase from '../app/supabaseClient';
+import supabase from '@app/supabaseClient';
 
 // Define el tipo de las tareas
 interface Task {
