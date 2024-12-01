cd096ee071d679443e79aff60f746eff7cbbe7af
César <cesarlpb@gmail.com>
2024-12-01 15:28:32 +0100

    rename de tab

---
+++ practica02/tasks-app/app/(tabs)/upload.tsx
@@ -0,0 +1,26 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function TabTwoScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.h2}>File Uploader</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#505050',
+    flex: 1,
+    display: 'flex',
+    alignItems: 'center',
+    alignContent: 'center',
+    height: 100
+  },
+  h2: {
+    color: '#f0f0f0',
+    marginTop: 32,
+    fontSize: 32,
+    fontWeight: 'bold'
+  }
+});
