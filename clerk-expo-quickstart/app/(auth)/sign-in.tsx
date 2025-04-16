4af88693172dac9d2b612a88e5ab5869e0c05ef7
César <cesarlpb@gmail.com>
2025-04-16 16:16:38 +0200

    fix del import de estilos

--- clerk-expo-quickstart/app/(auth)/sign-in.tsx
+++ clerk-expo-quickstart/app/(auth)/sign-in.tsx
@@ -2,7 +2,7 @@ import { useSignIn } from '@clerk/clerk-expo'
 import { Link, useRouter } from 'expo-router'
 import { Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React from 'react'
-import { styles } from './signInstyles'
+import { styles } from './signInStyles'
 
 export default function Page() {
   const { signIn, setActive, isLoaded } = useSignIn()
